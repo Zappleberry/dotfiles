0acb4d696218a828e2cac4b5591dad1591cd682f
Zappleberry <zlnelson@ucdavis.edu>
2018-10-25 11:17:16 -0700

    Added newsboat config

--- install-config.sh
+++ install-config.sh
@@ -7,15 +7,17 @@ ln -sv $download_dir/Xdefaults ~/.Xdefaults
 ln -sv $download_dir/Xmodmap ~/.Xmodmap
 ln -sv $download_dir/rofi-themes ~/.rofi-themes
 
-# linking configuration files
-ln -sv $download_dir/config/dunst ~/.config/dunst
-ln -sv $download_dir/config/feh ~/.config/feh
-ln -sv $download_dir/config/neofetch ~/.config/neofetch
-ln -sv $download_dir/config/openbox ~/.config/openbox
-ln -sv $download_dir/config/ranger ~/.config/ranger
-ln -sv $download_dir/config/rofi ~/.config/rofi
-ln -sv $download_dir/config/sxhkd ~/.config/sxhkd
-ln -sv $download_dir/config/zathura ~/.config/zathura
+# linking configuration files. Do not use because it causes infinite loop.
+ln -sTv $download_dir/config/dunst/ ~/.config/dunst
+ln -sTv $download_dir/config/feh/ ~/.config/feh
+ln -sTv $download_dir/config/neofetch/ ~/.config/neofetch
+ln -sTv $download_dir/config/openbox/ ~/.config/openbox
+ln -sTv $download_dir/config/ranger/ ~/.config/ranger
+ln -sTv $download_dir/config/rofi/ ~/.config/rofi
+ln -sTv $download_dir/config/sxhkd/ ~/.config/sxhkd
+ln -sTv $download_dir/config/zathura/ ~/.config/zathura
+ln -sTv $download_dir/config/newsboat/ ~/.newsboat # Needs further modification
 
 # linking spacemacs
+# This may become a .emacs.d in the future?
 ln -sv $download_dir/spacemacs ~/.spacemacs
