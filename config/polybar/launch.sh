f4e00e9607e32998866843b91ce06236853b4e05
Zappleberry <zlnelson@ucdavis.edu>
2019-07-02 07:12:29 -0700

    Don't output 'Disconnected' from vpn when not connected to network.

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -10,3 +10,4 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar -r ashes &
 
 echo "Polybar launched..."
+notify-send "Polybar Launched"
--- config/polybar/scripts/vpn-openvpn-nmcli.sh
+++ config/polybar/scripts/vpn-openvpn-nmcli.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 vpn=$(nmcli | grep "VPN connection" | tail -n 1 | sed "s/\sVPN\sconnection//g")
-if [ -z $vpn ]
+connected=$(nmcli d | awk '{print $3}' | grep connected | grep -v disconnected)
+
+if [ -z $vpn ] && [ -z $connected ]
+then
+	echo ""
+elif [ -z $vpn ]
 then
 	echo "%{F#ae95c7}%{F-} Disconnected"
 else
