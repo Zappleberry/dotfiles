a32fc8b2ec2fb8ac6e421c194c1878e5935754a4
Zappleberry <zlnelson@ucdavis.edu>
2019-07-09 12:43:02 -0700

    Added transmission script for bar

---
+++ config/polybar/scripts/transmission.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -z "$(pgrep -x transmission-da)" ]
+then
+	echo "td not running"
+else
+	transmission-remote -l | grep % | sed "
+	s/.*Stopped.*/A/g;
+	s/.*Seeding.*/Z/g;
+	s/.*100%.*/N/g;
+	s/.*Idle.*/B/g;
+	s/.*Uploading.*/L/g;
+	s/.*%.*/M/g" |
+	sort -h | uniq -c | sed " # Now we replace the standin letters with icons.
+				s/A/%\{F#aec795\}%\{F-\}/g;
+				s/B/%\{F#aec795\}%\{F-\}/g;
+				s/L/%\{F#aec795\}%\{F-\}/g;
+				s/M/%\{F#aec795\}%\{F-\}/g;
+				s/N/%\{F#aec795\}%\{F-\}/g;
+				s/Z/%\{F#aec795\}%\{F-\}/g" | awk '{print $2, $1}' | tr '\n' ' ' | sed -e "s/ $//g"
+fi
