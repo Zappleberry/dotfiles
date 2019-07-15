463b5345689e4b541d7d6a171acb8d448938c1dc
Zappleberry <zlnelson@ucdavis.edu>
2019-07-14 19:44:37 -0700

    Added emoji file for scripts

--- install-config.sh
+++ install-config.sh
@@ -14,6 +14,7 @@ ln -sfv $download_dir/vimrc ~/.vimrc
 ln -sfv $download_dir/octaverc ~/.octaverc
 ln -sfv $download_dir/bash_profile ~/.bash_profile
 ln -sfv $download_dir/profile ~/.profile
+ln -sfv $download_dir/emoji ~/.emoji
 
 # linking configuration files.
 ln -sTv $download_dir/config/dunst/ ~/.config/dunst
