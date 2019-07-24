a7bf30d3575d9cc3d40dc482ce6244fc27f84e6d
Zappleberry <zlnelson@ucdavis.edu>
2019-07-24 00:11:52 -0700

    Switching to WireGuard and some renaming fixes

---
+++ config/polybar/scripts/vpn-wireguard.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+vpn=$(wg show interfaces | head -n 1)
+connected=$(nmcli d | awk '{print $3}' | grep connected | grep -v disconnected)
+
+if [ -z $vpn ] && [ -z $connected ]
+then
+	# not connected to network or vpn
+	echo ""
+elif [ -z $vpn ]
+then
+	# connected to network but not vpn
+	echo " %{F#ae95c7}%{F-} "
+else
+	# Connected to vpn
+	echo "%{F#ae95c7}%{F-} $vpn"
+fi
