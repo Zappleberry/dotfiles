8bdb108b154bd9ac32dcbd573dcaa4d6d74ba0ae
Zappleberry <zlnelson@ucdavis.edu>
2019-07-26 13:40:26 -0700

    Changed character

--- config/polybar/scripts/transmission.sh
+++ config/polybar/scripts/transmission.sh
@@ -12,7 +12,7 @@ else
 	s/.*Uploading.*/L/g;
 	s/.*%.*/M/g" |
 	sort -h | uniq -c | sed " # Now we replace the standin letters with icons.
-				s/A/%\{F#aec795\}%\{F-\}/g;
+				s/A/%\{F#aec795\}%\{F-\}/g;
 				s/B/%\{F#aec795\}%\{F-\}/g;
 				s/L/%\{F#aec795\}%\{F-\}/g;
 				s/M/%\{F#aec795\}%\{F-\}/g;
