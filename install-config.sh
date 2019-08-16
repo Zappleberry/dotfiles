c3faf144318d95a805735e065b64ae910a211fef
Zappleberry <zlnelson@ucdavis.edu>
2019-08-16 09:48:17 -0700

    Changed install file to fit renaming

--- install-config.sh
+++ install-config.sh
@@ -5,44 +5,44 @@ download_dir=$(pwd)
 # When using ln -s on a directory, make sure that the option -T is invoked, otherwise you will get a infinite loop
 
 # linking Xdefaults, Xmodmap, and rofi-themes
-ln -sv $download_dir/Xdefaults ~/.Xdefaults
-ln -sv $download_dir/Xmodmap ~/.Xmodmap
-ln -sfv $download_dir/xinitrc ~/.xinitrc
-ln -sfTv $download_dir/rofi-themes ~/.rofi-themes
-ln -sfv $download_dir/bashrc ~/.bashrc
-ln -sfv $download_dir/vimrc ~/.vimrc
-ln -sfv $download_dir/octaverc ~/.octaverc
-ln -sfv $download_dir/bash_profile ~/.bash_profile
-ln -sfv $download_dir/bash_aliases ~/.bash_aliases
-ln -sfv $download_dir/profile ~/.profile
-ln -sfv $download_dir/emoji ~/.emoji
+ln -sfv $download_dir/.Xdefaults ~/.Xdefaults
+ln -sfv $download_dir/.Xmodmap ~/.Xmodmap
+ln -sfv $download_dir/.xinitrc ~/.xinitrc
+ln -sfTv $download_dir/.rofi-themes ~/.rofi-themes
+ln -sfv $download_dir/.bashrc ~/.bashrc
+ln -sfv $download_dir/.vimrc ~/.vimrc
+ln -sfv $download_dir/.octaverc ~/.octaverc
+ln -sfv $download_dir/.bash_profile ~/.bash_profile
+ln -sfv $download_dir/.bash_aliases ~/.bash_aliases
+ln -sfv $download_dir/.profile ~/.profile
+ln -sfv $download_dir/.emoji ~/.emoji
 
 # linking configuration files.
-ln -sTv $download_dir/config/dunst/ ~/.config/dunst
-ln -sTv $download_dir/config/feh/ ~/.config/feh
-ln -sTv $download_dir/config/neofetch/ ~/.config/neofetch
-ln -sfTv $download_dir/config/openbox/ ~/.config/openbox
+ln -sfTv $download_dir/.config/dunst/ ~/.config/dunst
+ln -sfTv $download_dir/.config/feh/ ~/.config/feh
+ln -sfTv $download_dir/.config/neofetch/ ~/.config/neofetch
+ln -sfTv $download_dir/.config/openbox/ ~/.config/openbox
 mkdir ~/.config/ranger
-ln -sv $download_dir/config/ranger/rc.conf ~/.config/ranger/rc.conf
-ln -sv $download_dir/config/ranger/rifle.conf ~/.config/ranger/rifle.conf
-ln -sv $download_dir/config/ranger/scope.sh ~/.config/ranger/scope.sh
-ln -sv $download_dir/config/ranger/commands.py ~/.config/ranger/commands.py
-ln -sv $download_dir/config/ranger/commands_full.py ~/.config/ranger/commands_full.py
-ln -sTv $download_dir/config/rofi/ ~/.config/rofi
-ln -sTv $download_dir/config/polybar/ ~/.config/polybar
-ln -sTv $download_dir/config/sxhkd/ ~/.config/sxhkd
-ln -sTv $download_dir/config/i3/ ~/.config/i3
-ln -sTv $download_dir/config/zathura/ ~/.config/zathura
-ln -sTv $download_dir/config/newsboat/ ~/.newsboat # Needs further modification
-ln -sfv $download_dir/config/urlview ~/.urlview
+ln -sfv $download_dir/.config/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -sfv $download_dir/.config/ranger/rifle.conf ~/.config/ranger/rifle.conf
+ln -sfv $download_dir/.config/ranger/scope.sh ~/.config/ranger/scope.sh
+ln -sfv $download_dir/.config/ranger/commands.py ~/.config/ranger/commands.py
+ln -sfv $download_dir/.config/ranger/commands_full.py ~/.config/ranger/commands_full.py
+ln -sfTv $download_dir/.config/rofi/ ~/.config/rofi
+ln -sfTv $download_dir/.config/polybar/ ~/.config/polybar
+ln -sfTv $download_dir/.config/sxhkd/ ~/.config/sxhkd
+ln -sfTv $download_dir/.config/i3/ ~/.config/i3
+ln -sfTv $download_dir/.config/zathura/ ~/.config/zathura
+ln -sfTv $download_dir/.config/newsboat/ ~/.newsboat # Needs further modification
+ln -sfv $download_dir/.config/urlview ~/.urlview
 
 # link fonts
 mkdir ~/.fonts
-ln -sTv $download_dir/fonts/misc/ ~/.fonts/misc
+ln -sfTv $download_dir/.fonts/misc/ ~/.fonts/misc
 
 # linking spacemacs
 # This may become a .emacs.d in the future?
-ln -sv $download_dir/spacemacs ~/.spacemacs
+ln -sfv $download_dir/.spacemacs ~/.spacemacs
 
 # link bar
 #mkdir ~/.bar
