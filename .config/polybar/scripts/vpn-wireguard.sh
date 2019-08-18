f89c15188b3b2a437ff9c90778d1af6fb610b5de
Zappleberry <zlnelson@ucdavis.edu>
2019-08-17 22:08:21 -0500

    Fixed issue with if statements when multiple wireguard connections are active

--- .config/polybar/scripts/vpn-wireguard.sh
+++ .config/polybar/scripts/vpn-wireguard.sh
@@ -3,11 +3,11 @@
 vpn=$(wg show interfaces | head -n 1)
 connected=$(nmcli d | awk '{print $3}' | grep connected | grep -v disconnected)
 
-if [ -z $vpn ] && [ -z $connected ]
+if [ -z "$vpn" ] && [ -z "$connected" ]
 then
 	# not connected to network or vpn
 	echo ""
-elif [ -z $vpn ]
+elif [ -z "$vpn" ]
 then
 	# connected to network but not vpn
 	echo " %{F#ae95c7}%{F-} "
