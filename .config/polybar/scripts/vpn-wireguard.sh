cf963a4ff415dd060eeca1bd5101c49b37a89c0a
Zappleberry <zlnelson@ucdavis.edu>
2019-09-23 13:04:34 -0700

    Changed how it behaves with disconnented network

--- .config/polybar/scripts/vpn-wireguard.sh
+++ .config/polybar/scripts/vpn-wireguard.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 vpn=$(wg show interfaces | head -n 1)
-connected=$(nmcli d | awk '{print $3}' | grep connected | grep -v disconnected)
+#connected=$(nmcli d | awk '{print $3}' | grep connected | grep -v disconnected)
 
-if [ -z "$vpn" ] && [ -z "$connected" ]
-then
-	# not connected to network or vpn
-	echo ""
-elif [ -z "$vpn" ]
+#if [ -z "$vpn" ] && [ -z "$connected" ]
+#then
+#	# not connected to network or vpn
+#	echo ""
+if [ -z "$vpn" ]
 then
 	# connected to network but not vpn
 	echo " %{F#ae95c7}%{F-} "
