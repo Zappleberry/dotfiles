79afe8f2a94e256aacc599f26e520d9df3078a16
Zappleberry <zlnelson@ucdavis.edu>
2020-02-29 21:25:33 -0800

    Modified master dotfiles to create dotfiles specific to XPS 13 9343

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Terminate already running bar instances
-killall -q polybar
+kill $(pgrep polybar)
+#killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
